0e7d4a59e2729f357f8dcc0835a098f36b514ac3
Achilleas <achmichael@gmail.com>
2023-02-20 11:28:05 +0200

    Add caching for property requests

--- server/controllers/propertyController.js
+++ server/controllers/propertyController.js
@@ -1,10 +1,13 @@
+const { createCache } = require('../services/cacheService');
 const { getAll, persistProperty } = require('../services/persistService');
+
+const propertyCache = createCache(600);
 
 const addNewProperty = async (req, res) => {
   const values = req.body;
-
   try {
     await persistProperty(values);
+    propertyCache.del('properties');
     return res.status(201).send();
   } catch (error) {
     console.log(error);
@@ -13,8 +16,15 @@ const addNewProperty = async (req, res) => {
 };
 
 const getAllproperties = async (_, res) => {
+  cachedProperties = propertyCache.get('properties');
+  if (cachedProperties) {
+    console.log('respond with cache');
+    return res.json(cachedProperties);
+  }
+
   try {
     const properties = await getAll();
+    propertyCache.set('properties', properties);
     return res.json(properties);
   } catch (error) {
     console.log(error);
